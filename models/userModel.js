f6aa7b0121993e581e530f2d5b187a517f210a7a
Henintsoa <118676248+ETU001778-W31-P15A-Henintsoa@users.noreply.github.com>
2025-08-14 14:34:17 +0300

    commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,22 +1,27 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const connexion = require('./Connexion');
+const generalisation = require('./Generalisation');
 
-const pool = new Pool({
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT,
-});
+const findUser = async (connexion, request) => {
+    let newconnexion = false;
 
-// Vérifier si un utilisateur existe déjà
-const findUser = async (identifiant, mail) => {
-    const result = await pool.query(
-        'SELECT * FROM Utilisateur WHERE identifiant = $1 OR mail = $2',
-        [identifiant, mail]
-    );
-    return result.rows[0];
+    if (!connexion) {
+        connexion = await connexion.pool.connect();
+        newconnexion = true;
+    }
+
+    try {
+        const result = await generalisation.getObject(connexion, request);
+        return result;
+    } catch (error) {
+        console.error('Error finding user:', error);
+        throw error;
+    } finally {
+        if (newconnexion) {
+            connexion.release();
+        }
+    }
 };
+
 
 const insertUser = async (identifiant, nom, prenom, mail, mdp, idtu) => {
     const result = await pool.query(
