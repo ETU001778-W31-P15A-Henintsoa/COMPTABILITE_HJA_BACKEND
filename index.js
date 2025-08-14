3173508f609e7575cb0a1923238f313fcd155f55
Henintsoa <118676248+ETU001778-W31-P15A-Henintsoa@users.noreply.github.com>
2025-08-14 13:52:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,32 +3,12 @@ const allparkings = require('./parkings.json');
 
 const app = express();
 
-// app.get('/parkings', (req,res) => {
-//     res.send("Liste des parkings")
-// })
+app.use(express.json());
 
-// app.post('/parkings', (req,res) => {res.status(200).json(allparkings)})
-// app.listen(8080, () => {console.log("Serveur à l'écoute")})
+// Routes
+app.use('/users', userRoutes);
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-// const parkings = allparkings.parkings;
-// app.get('/parkings', (req,res) => {res.status(200).json(parkings)});
-
-// app.get('/parkings/:id', (req,res) => {const id = parseInt(req.params.id);
-// const parking = parkings.find(parking => parking.id === id)
-// res.status(200).json(parking)});
-
-app.listen(8080, () => {  console.log("Serveur à l'écoute sur le port 8080")});
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+// app.listen(8080, () => {  console.log("Serveur à l'écoute sur le port 8080")});
 app.listen(process.env.PORT, () => {
     console.log(`🚀 Serveur démarré sur http://localhost:${process.env.PORT}`);
 });
