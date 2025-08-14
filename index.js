fe3986ceab74f0c2bf658cd6711117c2e9fcdb7e
Henintsoa <118676248+ETU001778-W31-P15A-Henintsoa@users.noreply.github.com>
2025-08-14 13:54:23 +0300

    commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+<<<<<<< Updated upstream
 const allparkings = require('./parkings.json');
 
+=======
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+>>>>>>> Stashed changes
 const app = express();
 
 app.use(express.json());
@@ -8,7 +13,6 @@ app.use(express.json());
 // Routes
 app.use('/users', userRoutes);
 
-// app.listen(8080, () => {  console.log("Serveur à l'écoute sur le port 8080")});
 app.listen(process.env.PORT, () => {
     console.log(`🚀 Serveur démarré sur http://localhost:${process.env.PORT}`);
 });
